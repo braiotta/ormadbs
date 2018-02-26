b368e5be69ffe2dd8e55c957e59a5563e25514e6
braiotta <braiotta@mit.edu>
2018-02-26 17:03:39 -0500

    added event_stats

---
+++ event_stats.py
@@ -0,0 +1,16 @@
+from objects.ActionNetwork import ActionNetwork
+from collections import OrderedDict
+import operator
+
+a = ActionNetwork()
+a.load()
+
+events = {}
+for row in a.rows:
+    event = str(row['event']).strip()
+    if event not in events:
+        events[event] = 0
+    events[event] += 1
+
+events = OrderedDict(sorted(events.items(), key=operator.itemgetter(1), reverse=True))
+[print(row, ':', count) for row, count in events.items()]
